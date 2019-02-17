c2c893bce2b315462779354e1e66e8692f6f8000
Mr Node <node@kgcoe-st-project.se.rit.edu>
2019-02-17 18:19:44 -0500

    Add package.json

--- createTables.js
+++ createTables.js
@@ -5,7 +5,8 @@ const db = require('./db.js');
 createTables();
 
 async function createTables(){
-	await db.query("");
+	const {rows} = await db.query("SELECT * FROM experiments");
+	console.log(rows);
 	console.log("Table creation finished properly");
 	process.exit();
 }
