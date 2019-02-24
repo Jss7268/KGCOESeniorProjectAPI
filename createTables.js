c557d1e2e4515a830c263655d19d9bb5989dcf07
MukeytheMuse <MukeytheMuse@users.noreply.github.com>
2019-02-24 01:15:54 -0500

    Successfully creates all tables
    and associated columns. The column "user" in the "user_inputs" and "user_experiments" tables had to be changed to "username" as leaving it as "user" produced an error. This has been updated in the database schema as well.

--- createTables.js
+++ createTables.js
@@ -4,16 +4,47 @@ const db = require('./db.js');
 
 createTables();
 
-async function createTables()
-	const users = await db.query("CREATE TABLE users (id uuid PRIMARY KEY, name VARCHAR (100) NOT NULL, email VARCHAR (255) UNIQUE NOT NULL, hashedPassword VARCHAR (255) NOT NULL, createdAt bigint NOT NULL, updateAt bigint, deletedAt bigint)");
-	const experiments = await db.query("CREATE TABLE experiments (id uuid PRIMARY KEY, creator users NOT NULL, createdAt bigint NOT NULL, updateAt bigint, deletedAt bigint, startTime bigint)");
-	const users_experiments = await db.query("CREATE TABLE users_experiments (experiment experiments NOT NULL, user users NOT NULL, createdAt bigint NOT NULL, updatedAt bigint, deletedAt bigint)");
-	const supported_devices = await db.query("CREATE TABLE supported_devices (id uuid PRIMARY KEY, permission BOOLEAN, createdAt bigint NOT NULL, updatedAt bigint, deletedAt bigint)");
-	const devices_experiments = await db.query("CREATE TABLE devices_experiments (experiment experiments NOT NULL, device supported_devices NOT NULL, createdAt bigint NOT NULL, updatedAt bigint, deletedAt bigint)");
-	const output_types = await db.query("CREATE TABLE output_types (id uuid PRIMARY KEY, name VARCHAR (100) NOT NULL, units VARCHAR (100) NOT NULL, createdAt bigint NOT NULL, updateAt bigint, deletedAt bigint)");
-	const device_outputs = await db.query("");
-	const user_inputs = await db.query("");
-	console.log(rows);
-	console.log(users);
+async function createTables(){
+	try {
+		await db.query("CREATE TABLE users (id uuid PRIMARY KEY, name VARCHAR (100) NOT NULL, email VARCHAR (255) UNIQUE NOT NULL, hashedPassword VARCHAR (255) NOT NULL, createdAt bigint NOT NULL, updateAt bigint, deletedAt bigint)");
+	} catch (err) {
+		console.log(err);
+	}
+	try {
+		await db.query("CREATE TABLE experiments (id uuid PRIMARY KEY, creator users NOT NULL, createdAt bigint NOT NULL, updateAt bigint, deletedAt bigint, startTime bigint)");
+	} catch (err) {
+		console.log(err);
+	}
+	try {
+		await db.query("CREATE TABLE users_experiments (experiment experiments NOT NULL, username users NOT NULL, createdAt bigint NOT NULL, updatedAt bigint, deletedAt bigint)");
+	} catch (err) {
+		console.log(err);
+	}
+	try {
+		await db.query("CREATE TABLE supported_devices (id uuid PRIMARY KEY, permission BOOLEAN, createdAt bigint NOT NULL, updatedAt bigint, deletedAt bigint)");
+	} catch (err) {
+		console.log(err);
+	}
+	try {
+		await db.query("CREATE TABLE devices_experiments (experiment experiments NOT NULL, device supported_devices NOT NULL, createdAt bigint NOT NULL, updatedAt bigint, deletedAt bigint)");
+	} catch (err) {
+		console.log(err);
+	}
+	try {
+		await db.query("CREATE TABLE output_types (id uuid PRIMARY KEY, name VARCHAR (100) NOT NULL, units VARCHAR (100) NOT NULL, createdAt bigint NOT NULL, updateAt bigint, deletedAt bigint)");
+	} catch (err) {
+		console.log(err);
+	}
+	try {
+		await db.query("CREATE TABLE device_outputs (output_type output_types NOT NULL, experiment_id experiments NOT NULL, device_id supported_devices NOT NULL, output_value decimal NOT NULL, timestamp bigint NOT NULL, createdAT bigint NOT NULL, updatedAt bigint, deletedAt bigint)");
+	} catch (err) {
+		console.log(err);
+	}
+	try {
+		await db.query("CREATE TABLE user_inputs (description VARCHAR (255) NOT NULL, timestamp bigint NOT NULL, experiment_id experiments NOT NULL, device_id supported_devices NOT NULL, username users NOT NULL, createdAT bigint NOT NULL, updatedAt bigint, deletedAt bigint)");
+	} catch (err) {
+		console.log(err);
+	}
 	console.log("Table creation finished properly");
 	process.exit();
+}
