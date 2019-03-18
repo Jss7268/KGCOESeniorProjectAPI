020004cf803c79e5129bdba807185f0173efd3d8
iamnukee <pto7724@rit.edu>
2019-03-18 16:13:37 -0400

    server file corresponding to last commit for the api

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 const https = require("https"),
   fs = require("fs");
 
+const db = require('./db.js');
+
 const options = {
   key: fs.readFileSync("/etc/letsencrypt/live/kgcoe-st-project.se.rit.edu/privkey.pem"),
   cert: fs.readFileSync("/etc/letsencrypt/live/kgcoe-st-project.se.rit.edu/fullchain.pem")
@@ -9,11 +11,35 @@ const options = {
 
 const app = express();
 
-app.use((req, res) => {
-  res.writeHead(200);
-  res.end("hello world\n");
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT;
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
 });
 
+app.get('/', function (req, res){
+  return res.status(200).send({'message': 'YES you got this!!'});
+});
+
+router.get('/getexperiments', db.getExperiments);
+
+router.get('/getexperimentdata/:id', db.getExperimentDataById);
+
+app.use('/api', router);
+
+//start server
 app.listen(8000);
 
 https.createServer(options, app).listen(8080);
