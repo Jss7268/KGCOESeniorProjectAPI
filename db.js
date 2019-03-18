e654e920ba8db0d9e2b939397166340559be23f3
iamnukee <pto7724@rit.edu>
2019-03-18 16:12:07 -0400

    Api for getting all experiments and getting an individual experiement by id

--- db.js
+++ db.js
@@ -4,17 +4,40 @@ const pool = new Pool({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASS,
-	database: process.env.DB_DATABASE
+	database: process.env.DB_DATABASE,
+	port: 5432,
 });
 
-pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err)
-  process.exit(-1)
-})
+const getExperiments = (request, response) => {
+	pool.query('SELECT * FROM experiments', (error, results) => {
+	  if (error) {
+		throw error;
+		console.log(error);
+	  }
+	  response.status(200).json(results.rows);
+	});
+}
+
+// const getExperimentsDates = (request, response) => {
+// 	pool.query('SELECT DATE FROM experiments WHERE ')
+// }
+
+const getExperimentDataById = (request, response) => {
+	const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM device_outputs WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+}
 
 pool.setMaxListeners(0);
 
 module.exports = {
 	query: (text, params) => pool.query(text, params),
-	pool: pool
+	pool: pool,
+	getExperiments,
+	getExperimentDataById
 }
