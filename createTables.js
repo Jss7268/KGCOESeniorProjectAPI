66e74ebce96291eb66f7bf568070bcd95765f8c3
Iceemrr <Iceemrr@aim.com>
2019-04-10 16:18:46 -0400

    Made some more changes to create tables, all uuids and snake_case

--- createTables.js
+++ createTables.js
@@ -4,39 +4,93 @@ const db = require('./config/db.js');
 
 createTables();
 
-async function createTables(){
+async function createTables() {
 	try {
 		await db.query("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";");
 	} catch (err) {
 		console.log(err);
 	}
 	try {
-		await db.query("CREATE TABLE users (id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (), name VARCHAR (100) NOT NULL, email VARCHAR (255), hashedPassword VARCHAR (255) NOT NULL, access smallint NOT NULL DEFAULT 0, createdAt bigint NOT NULL, updatedAt bigint, deletedAt bigint)");
+		await db.query("CREATE TABLE IF NOT EXISTS users " +
+			"(id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (), " + 
+			"name VARCHAR (100) NOT NULL, " + 
+			"email VARCHAR (255), " + 
+			"hashed_password VARCHAR (255) NOT NULL, " + 
+			"access smallint NOT NULL DEFAULT 0, " + 
+			"created_at bigint NOT NULL, " + 
+			"updated_at bigint, " + 
+			"deleted_At bigint NOT NULL DEFAULT 0)");
 	} catch (err) {
 		console.log(err);
 	}
 	try {
-		await db.query("CREATE TABLE experiments (id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (), creatorId uuid, createdAt bigint NOT NULL, updatedAt bigint, deletedAt bigint, startTime bigint, FOREIGN KEY (creatorId) REFERENCES users (id))");
+		await db.query("CREATE TABLE IF NOT EXISTS experiments " +
+			"(id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (), " + 
+			"creator_id uuid, " + 
+			"notes text, " +
+			"description text, " +
+ 			"created_at bigint NOT NULL, " + 
+			"updated_at bigint, " + 
+			"deleted_at bigint NOT NULL DEFAULT 0, " + 
+			"startTime bigint, " + 
+			"FOREIGN KEY (creator_id) REFERENCES users (id))");
 	} catch (err) {
 		console.log(err);
 	}
 	try {
-		await db.query("CREATE TABLE users_experiments (experiment experiments NOT NULL, username users NOT NULL, createdAt bigint NOT NULL, updatedAt bigint, deletedAt bigint)");
+		await db.query("CREATE TABLE IF NOT EXISTS devices_experiments " +
+			"(experiment_id uuid NOT NULL, " + 
+			"device_id uuid NOT NULL, " + 
+			"created_at bigint NOT NULL, " + 
+			"updated_at bigint, " + 
+			"deleted_at bigint NOT NULL DEFAULT 0, " + 
+			"PRIMARY KEY (experiment_id, device_id, deleted_at), " +
+			"FOREIGN KEY (device_id) REFERENCES users (id), " + 
+			"FOREIGN KEY (experiment_id) REFERENCES experiments (id))");
 	} catch (err) {
 		console.log(err);
 	}
 	try {
-		await db.query("CREATE TABLE output_types (id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (), name VARCHAR (100) NOT NULL, units VARCHAR (100) NOT NULL, createdAt bigint NOT NULL, updatedAt bigint, deletedAt bigint)");
+		await db.query("CREATE TABLE  IF NOT EXISTS output_types " +
+			"(id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (), " + 
+			"name VARCHAR (100) NOT NULL, " + 
+			"units VARCHAR (100) NOT NULL, " + 
+			"created_at bigint NOT NULL, " + 
+			"updated_at bigint, " + 
+			"deleted_at bigint NOT NULL DEFAULT 0)");
 	} catch (err) {
 		console.log(err);
 	}
 	try {
-		await db.query("CREATE TABLE device_outputs (device_id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (), output_type output_types NOT NULL, experiment_id experiments NOT NULL, output_value decimal NOT NULL, timestamp bigint NOT NULL, createdAT bigint NOT NULL, updatedAt bigint, deletedAt bigint)");
+		await db.query("CREATE TABLE IF NOT EXISTS device_outputs " +
+			"(device_id uuid NOT NULL, " + 
+			"output_type_id uuid NOT NULL, " + 
+			"experiment_id uuid NOT NULL, " + 
+			"output_value decimal NOT NULL, " + 
+			"timestamp bigint NOT NULL, " + 
+			"created_at bigint NOT NULL, " + 
+			"updated_at bigint, " + 
+			"deleted_at bigint NOT NULL DEFAULT 0, " + 
+			"PRIMARY KEY (device_id, output_type_id, experiment_id, deleted_at), " +
+			"FOREIGN KEY (device_id) REFERENCES users (id), " + 
+			"FOREIGN KEY (output_type_id) REFERENCES output_types (id), " + 
+			"FOREIGN KEY (experiment_id) REFERENCES experiments (id))");
 	} catch (err) {
 		console.log(err);
 	}
 	try {
-		await db.query("CREATE TABLE user_inputs (description VARCHAR (255) NOT NULL, timestamp bigint NOT NULL, experiment_id experiments NOT NULL, device_id uuid, username users NOT NULL, createdAT bigint NOT NULL, updatedAt bigint, deletedAt bigint, FOREIGN KEY (device_id) REFERENCES device_outputs (device_id))");
+		await db.query("CREATE TABLE IF NOT EXISTS user_inputs " +
+			"(description VARCHAR (255) NOT NULL, " +
+			"timestamp bigint NOT NULL, " +
+			"experiment_id uuid NOT NULL, " +
+			"device_id uuid NOT NULL, " + 
+			"user_id uuid NOT NULL, " + 
+			"created_at bigint NOT NULL, " + 
+			"updated_at bigint, " + 
+			"deleted_At bigint NOT NULL DEFAULT 0, " + 
+			"FOREIGN KEY (experiment_id) REFERENCES experiments (id), " + 
+			"FOREIGN KEY (device_id) REFERENCES users (id), " + 
+			"FOREIGN KEY (user_id) REFERENCES users (id))");
 	} catch (err) {
 		console.log(err);
 	}
