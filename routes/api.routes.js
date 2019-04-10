8a1e7ebe240393286c2bcfd1e41942b077eccf41
Iceemrr <Iceemrr@aim.com>
2019-04-10 18:11:02 -0400

    Added 'fake' outline for device_output files

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -9,6 +9,8 @@ router.use(require('./auth.routes'));
 
 // API v1
 router.use('/v1', require('./v1/users.routes'));
+router.use('/v1', require('./v1/experiments.routes'));
+router.use('/v1', require('./v1/device_outputs.routes'));
 
 // API Error routes
 router.use(function(req, res) {
