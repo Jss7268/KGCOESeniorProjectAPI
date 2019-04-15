afc03e2463a3538116b026cde5ddf99e7f9c040c
Iceemrr <Iceemrr@aim.com>
2019-04-14 21:02:46 -0400

    Add single get by id for device outputs

--- routes/v1/device_outputs.routes.js
+++ routes/v1/device_outputs.routes.js
@@ -6,7 +6,7 @@ router.post('/device_outputs', deviceOutputsController.createDeviceOutput);
 router.get('/device_outputs/experiment/:experiment_id', deviceOutputsController.listDeviceOutputsByExperiment);
 router.get('/device_outputs/device/:device_id', deviceOutputsController.listDeviceOutputsByDevice);
 router.get('/device_outputs/:device_id/:experiment_id', deviceOutputsController.listDeviceOutputsByDeviceExperiment);
-
+router.get('/device_outputs/:id', deviceOutputsController.getOneDeviceOutput);
 router.put('/device_outputs/:experiment_id/:device_id/:output_type_id/output_value', deviceOutputsController.changeOutputValue);
 router.get('/device_outputs/:experiment_id/:device_id/:output_type_id', deviceOutputsController.getOneDeviceOutput);
 router.delete('/device_outputs/:experiment_id/:device_id/:output_type_id', deviceOutputsController.deleteDeviceOutput);
