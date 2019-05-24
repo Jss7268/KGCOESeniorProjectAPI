adddb167c50aa5f47005a75e934abed2a746b1fc
Iceemrr <jss7268@rit.edu>
2019-05-24 17:53:11 -0400

    Tests for create update delete device outputs

--- test/controllers/device_outputs.controller.spec.js
+++ test/controllers/device_outputs.controller.spec.js
@@ -1,77 +1,144 @@
-const mocks = require('../mocks');
+const Mocks = require('../mocks');
 const chai = require('chai');
 const sinon = require('sinon');
 chai.use(require('sinon-chai'));
 const expect = chai.expect;
-const errorMessage = 'error';
-deviceOutputsController = require('../../controllers/device_outputs.controller')
+DeviceOutputsController = require('../../controllers/device_outputs.controller')
+
+const ERROR_MESSAGE = 'error';
+const RESULT = { result: 'result' };
 
 var req;
 var res;
 var deviceOutput;
 var badDeviceOutput;
-var berifier;
+var verifier;
 var badVerifier;
 
 // use function instead of lambda
 // https://mochajs.org/#arrow-functions
-before(function() {
+before(function () {
+  req = Mocks.mockRequest();
   deviceOutput = {
-    create: (_) => { return { id: 1 } }
+    create: (ignore) => { return { id: Mocks.ID } },
+    updateOutputValue: ({ id, output_value }) => { return { id: id, output_value: output_value } },
+    delete: (ignore) => { return {id: Mocks.ID } },
   }
   badDeviceOutput = {
-    create: (_) => { throw {message: errorMessage} }
+    create: (ignore) => { throw { message: ERROR_MESSAGE } },
+    updateOutputValue: (ignore) => { throw { message: ERROR_MESSAGE } },
+    delete: (ignore) => { throw { message: ERROR_MESSAGE } },
+
+
   }
-  req = mocks.mockRequest({ decoded: { accessLevel: null }, body: {} });
-  berifier = {
-    verifyMinAccessName: (a, b) => new Promise((resolve) => resolve({}))
+  verifier = {
+    verifyMinAccessName: (ignore1, ignore2) => new Promise((resolve) => resolve({}))
   };
   badVerifier = {
-    verifyMinAccessName: (a, b) => new Promise((_, reject) => reject({ message: errorMessage }))
+    verifyMinAccessName: (ignore1, ignore2) => new Promise((ignore, reject) => reject({ message: ERROR_MESSAGE }))
   };
 });
 
-beforeEach(function() {
-  res = mocks.mockResponse();
+beforeEach(function () {
+  res = Mocks.mockResponse();
 });
 
-describe('createDeviceOutput', function() {
-  it('returns 400 status on bad verifier', function(done) {
-    deviceOutputsController.createDeviceOutput(deviceOutput, badVerifier)(req, res)
+describe('createDeviceOutput', function () {
+  it('returns 400 status on bad verifier', function (done) {
+    DeviceOutputsController.createDeviceOutput(deviceOutput, badVerifier)(req, res)
       .then(() => {
         expect(res.status).to.have.been.calledWith(400);
-        expect(res.json).to.have.been.calledWith({ message: errorMessage });
+        expect(res.json).to.have.been.calledWith({ message: ERROR_MESSAGE });
         done();
       })
       .catch((err) => done(err));
   });
 
-  it('returns 400 status on bad create', function(done) {
-    deviceOutputsController.createDeviceOutput(deviceOutput, badVerifier)(req, res)
+  it('returns 400 status on bad create', function (done) {
+    DeviceOutputsController.createDeviceOutput(badDeviceOutput, verifier)(req, res)
       .then(() => {
         expect(res.status).to.have.been.calledWith(400);
-        expect(res.json).to.have.been.calledWith({ message: errorMessage });
+        expect(res.json).to.have.been.calledWith({ message: ERROR_MESSAGE });
         done();
       })
       .catch((err) => done(err));
   });
 
-  it('returns 200 status on error', function(done) {
-    deviceOutputsController.createDeviceOutput(deviceOutput, berifier)(req, res)
+  it('returns 200 status on error', function (done) {
+    DeviceOutputsController.createDeviceOutput(deviceOutput, verifier)(req, res)
       .then(() => {
         expect(res.status).to.have.been.calledWith(200);
-        expect(res.json).to.have.been.calledWith({ message: 'success! created new device_output', id: 1 });
+        expect(res.json).to.have.been.calledWith({ message: 'success! created new device_output', id: Mocks.ID });
         done();
       })
       .catch((err) => done(err));
   });
 });
+
 describe('changeOutputValue', () => {
+  it('returns 400 status on bad verifier', function (done) {
+    DeviceOutputsController.changeOutputValue(deviceOutput, badVerifier)(req, res)
+      .then(() => {
+        expect(res.status).to.have.been.calledWith(400);
+        expect(res.json).to.have.been.calledWith({ message: ERROR_MESSAGE });
+        done();
+      })
+      .catch((err) => done(err));
+  });
 
+  it('returns 400 status on bad create', function (done) {
+    DeviceOutputsController.changeOutputValue(badDeviceOutput, verifier)(req, res)
+      .then(() => {
+        expect(res.status).to.have.been.calledWith(400);
+        expect(res.json).to.have.been.calledWith({ message: ERROR_MESSAGE });
+        done();
+      })
+      .catch((err) => done(err));
+  });
+
+  it('returns 200 status on error', function (done) {
+    DeviceOutputsController.changeOutputValue(deviceOutput, verifier)(req, res)
+      .then(() => {
+        expect(res.status).to.have.been.calledWith(200);
+        expect(res.json).to.have.been.calledWith({ id: Mocks.ID, output_value: Mocks.OUTPUT_VALUE});
+        done();
+      })
+      .catch((err) => done(err));
+  });
 });
+
 describe('deleteDeviceOutput', () => {
+  it('returns 400 status on bad verifier', function (done) {
+    DeviceOutputsController.deleteDeviceOutput(deviceOutput, badVerifier)(req, res)
+      .then(() => {
+        expect(res.status).to.have.been.calledWith(400);
+        expect(res.json).to.have.been.calledWith({ message: ERROR_MESSAGE });
+        done();
+      })
+      .catch((err) => done(err));
+  });
 
+  it('returns 400 status on bad create', function (done) {
+    DeviceOutputsController.deleteDeviceOutput(badDeviceOutput, verifier)(req, res)
+      .then(() => {
+        expect(res.status).to.have.been.calledWith(400);
+        expect(res.json).to.have.been.calledWith({ message: ERROR_MESSAGE });
+        done();
+      })
+      .catch((err) => done(err));
+  });
+
+  it('returns 200 status on error', function (done) {
+    DeviceOutputsController.deleteDeviceOutput(deviceOutput, verifier)(req, res)
+      .then(() => {
+        expect(res.status).to.have.been.calledWith(200);
+        expect(res.json).to.have.been.calledWith({ message: 'deleted device_output with id: ' + Mocks.ID});
+        done();
+      })
+      .catch((err) => done(err));
+  });
 });
+
 describe('getOneDeviceOutput', () => {
 
 });
--- test/mocks.js
+++ test/mocks.js
@@ -1,4 +1,9 @@
 const sinon = require('sinon');
+
+const OUTPUT_VALUE = 100;
+const DEVICE_ID = 20;
+const ID = 1234;
+
 module.exports = {
     mockResponse: () => {
         const res = {};
@@ -6,7 +11,19 @@ module.exports = {
         res.json = sinon.stub().returns(res);
         return res;
     },
-    mockRequest: (req) => {
-        return req;
-    }
+    mockRequest: () => {
+        return {
+            body: {
+                output_value: OUTPUT_VALUE,
+                device_id: DEVICE_ID,
+
+            },
+            decoded: { accessLevel: null },
+            params: {id: ID}
+        };
+    },
+    DEVICE_ID: DEVICE_ID,
+    OUTPUT_VALUE: OUTPUT_VALUE,
+    ID: ID,
+
 }
