72a11d5b426d99eff32847b9b39ea68c973daef0
iamnukee <pto7724@rit.edu>
2019-06-02 12:22:56 -0400

    Capitalize controller variable

--- routes/v1/user_inputs.routes.js
+++ routes/v1/user_inputs.routes.js
@@ -1,14 +1,14 @@
 var router = require('express').Router();
 const Injector = require('../../config/injector')
-const userInputsController = Injector.UserInputsController;
+const UserInputsController = Injector.UserInputsController;
 
-router.get('/user_inputs',  userInputsController.listUserInputs);
-router.post('/user_inputs', userInputsController.createUserInput);
-router.get('/user_inputs/experiment/:experiment_id', userInputsController.listUserInputsByExperiment);
-router.get('/user_inputs/device/:device_id', userInputsController.listUserInputsByDevice);
-router.get('/user_inputs/:id', userInputsController.getOneUserInput);
-router.delete('/user_inputs/:id', userInputsController.deleteUserInput);
-router.post('/user_inputs/description', userInputsController.changeDescription);
-// router.get('/user_inputs/:device_id/:experiment_id', userInputsController.listUserInputsByDeviceExperiment); //needed?
+router.get('/user_inputs',  UserInputsController.listUserInputs);
+router.post('/user_inputs', UserInputsController.createUserInput);
+router.get('/user_inputs/experiment/:experiment_id', UserInputsController.listUserInputsByExperiment);
+router.get('/user_inputs/device/:device_id', UserInputsController.listUserInputsByDevice);
+router.get('/user_inputs/:id', UserInputsController.getOneUserInput);
+router.delete('/user_inputs/:id', UserInputsController.deleteUserInput);
+router.post('/user_inputs/description', UserInputsController.changeDescription);
+// router.get('/user_inputs/:device_id/:experiment_id', UserInputsController.listUserInputsByDeviceExperiment); //needed?
 
 module.exports = router;
