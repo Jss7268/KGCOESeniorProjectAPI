05fee01ec07364b18a5703cb1a00221148669c33
Michael Lehning <mll8657@rit.edu>
2019-06-30 05:16:58 -0400

    added what are functionally IN and NOT IN operators for query parameters

--- validators/validator.js
+++ validators/validator.js
@@ -131,7 +131,24 @@ module.exports = (Generic) => {
             if (operator.endsWith('NULL')) {
                 additionalWhere += ` AND ${column} ${operator}`;
                 delete data[key];
-            } else {
+            }
+            else if (operator == 'IN') {
+                // if only 1 parameter is supplied we need to turn it into a list
+                if (typeof data[key] == 'string') {
+                    data[key] = [data[key]];
+                }
+                additionalWhere += ` AND ${column} = ANY($${i})`
+                i++;
+            }
+            else if (operator == 'NOT IN') {
+                // if only 1 parameter is supplied we need to turn it into a list
+                if (typeof data[key] == 'string') {
+                    data[key] = [data[key]];
+                }
+                additionalWhere += ` AND ${column} != ALL($${i})`
+                i++;
+            }
+             else {
                 additionalWhere += ` AND ${column} ${operator} $${i}`;
                 i++;
             }
