3e26a4361360fe34deff40c6ea2ca97f7efb3359
Iceemrr <jss7268@rit.edu>
2019-07-07 23:10:29 -0400

    Remove all constant values from output

--- models/device_output.js
+++ models/device_output.js
@@ -12,9 +12,9 @@ module.exports = (db, Validator, DeviceExperiment) => {
                 = _Validator.getWhereAndQueryParamList(data, POSSIBLE_QUERY_PARAMS);
             return new Promise((resolve, reject) => {
                 _db.query(`SELECT *, device_outputs.* FROM device_outputs
-                        INNER JOIN output_types on device_outputs.output_type_id = output_types.id
-                        INNER JOIN sanitized_users on (device_outputs.device_id = sanitized_users.id)
-                        INNER JOIN experiments on (device_outputs.experiment_id = experiments.id)
+                        ${'output_type_id' in data ? '' : 'INNER JOIN output_types on device_outputs.output_type_id = output_types.id'}
+                        ${'device_id' in data ? '' : 'INNER JOIN sanitized_users on (device_outputs.device_id = sanitized_users.id)'}
+                        ${'experiment_id' in data ? '' : 'INNER JOIN experiments on (device_outputs.experiment_id = experiments.id)'}
                         WHERE device_outputs.deleted_at = 0
                         ${additionalWhere}
                         ORDER BY device_outputs.timestamp ASC`, queryParamList)
